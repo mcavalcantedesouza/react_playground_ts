9555f03abb6832b609189d507c3a1572107f7660
michel <michelc.desouza@gmail.com>
2024-05-16 17:25:33 -0300

    react playground

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,27 @@
+﻿import { useState } from "react";
+
+function Contador() {
+
+  const [valor, setValor] = useState(0);
+
+  function handleClick(){
+    setValor(valor + 2);
+  }
+
+  // console.log("Variável valor: " + valor);
+
+  return (
+    <div>
+
+      <h2>Componente Contador</h2>
+
+      <p> O valor atual é: {valor}</p>
+
+      <button onClick={handleClick}>Adicionar 2</button>
+
+    </div>
+  )
+ 
+}
+
+export default Contador
